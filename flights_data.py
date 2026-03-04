efc5fde78f1746375eb0cd9893c774b37a9ba670
Torben Abend <torben.abend@gmail.com>
2026-03-04 14:47:16 +0100

    Add option to show delayed flights by airport

--- flights_data.py
+++ flights_data.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, text
 QUERY_FLIGHT_BY_ID = "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id WHERE flights.ID = :id"
 QUERY_FLIGHT_BY_DATE = "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id WHERE flights.YEAR = :year AND flights.MONTH = :month AND flights.DAY = :day"
 QUERY_DELAYED_FLIGHTS_BY_AIRLINE = "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id WHERE airlines.AIRLINE = :airline AND flights.DEPARTURE_DELAY >= 20"
+QUERY_DELAYED_FLIGHTS_BY_AIRPORT = "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id WHERE flights.ORIGIN_AIRPORT = :airport AND flights.DEPARTURE_DELAY >= 20"
 
 # Define the database URL
 DATABASE_URL = "sqlite:///data/flights.sqlite3"
@@ -52,4 +53,12 @@ def get_delayed_flights_by_airline(airline):
     """
     params = {"airline": airline}
     return execute_query(QUERY_DELAYED_FLIGHTS_BY_AIRLINE, params)
-    
+
+
+def get_delayed_flights_by_airport(airport):
+    """
+    Searches for delayed flights by airport.
+    If flights were found, returns a list with all found records.
+    """
+    params = {"airport": airport}
+    return execute_query(QUERY_DELAYED_FLIGHTS_BY_AIRPORT, params)
