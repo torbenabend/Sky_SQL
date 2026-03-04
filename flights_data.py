2d464aff77ffb8e9946667ac7db8ed99fc2488b7
Torben Abend <torben.abend@gmail.com>
2026-03-04 14:28:54 +0100

    Add option to show flights by date

--- flights_data.py
+++ flights_data.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 
 QUERY_FLIGHT_BY_ID = "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id WHERE flights.ID = :id"
+QUERY_FLIGHT_BY_DATE = "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY FROM flights JOIN airlines ON flights.airline = airlines.id WHERE flights.YEAR = :year AND flights.MONTH = :month AND flights.DAY = :day"
 
 # Define the database URL
 DATABASE_URL = "sqlite:///data/flights.sqlite3"
@@ -30,7 +31,16 @@ def get_flight_by_id(flight_id):
     Searches for flight details using flight ID.
     If the flight was found, returns a list with a single record.
     """
-    params = {'id': flight_id}
+    params = {"id": flight_id}
     return execute_query(QUERY_FLIGHT_BY_ID, params)
+
+
+def get_flights_by_date(day, month, year):
+    """
+    Searches for flight details using flight date.
+    If flights were found, returns a list with all found records.
+    """
+    params = {"day": day, "month": month, "year": year}
+    return execute_query(QUERY_FLIGHT_BY_DATE, params)
 
     
